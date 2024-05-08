bddd3bd3a5c87c1f1677a9ea79ceb0eb4fed8563
Cruellest <gabriel81523642@gmail.com>
2024-05-08 16:34:07 -0400

    frequencia_caracteres finished

---
+++ frequencia_caracteres.cpp
@@ -0,0 +1,59 @@
+#include <cstdio>
+#include <map>
+
+using namespace std;
+
+class Lista_Caracteres{
+
+    private:
+    FILE *file;
+
+    public:
+    map<char,int> letras;
+
+    Lista_Caracteres(FILE *f){
+        file = f;
+        contarLetras();
+    };
+
+    void contarLetras(){
+        int ascii;
+        char letra;
+        while((ascii = fgetc(file)) != EOF){
+
+        letra = (char)ascii;
+
+            if (letras.find(letra) != letras.end()){
+                letras[letra] = letras[letra] + 1;
+            }
+            else{
+                letras[letra] = 1;
+            }
+            
+        }
+    }
+
+void printLetras(){
+    for(const auto &par : letras) {
+        printf("Letra:  %c, Frequencia: %d\n", par.first, par.second);
+    }
+}
+
+};
+
+
+int main(int argc, char const *argv[])
+{
+
+    FILE *original;
+
+    original= fopen("original.txt", "rb");
+
+    Lista_Caracteres lista(original);
+
+    lista.printLetras();
+
+
+
+    return 0;
+}
